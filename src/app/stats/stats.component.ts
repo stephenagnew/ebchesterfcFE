c568b1740de12f43f2c13621cc03d993a590daad
Stephen.p.agnew <Stephen.p.agnew@gmail.com>
2018-11-14 15:59:28 +0000

    edit stat sort to make a deep copy of array

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -23,9 +23,9 @@ export class StatsComponent implements OnInit {
     this.playerService.getPlayers()
     .subscribe(res => {
       this.players = res;
-      this.playerGoals = this.players.sort((a,b) => {return b.playerSeasonStats.goals - a.playerSeasonStats.goals})
-      this.playerAssists = this.players.sort((a,b) => {return b.playerSeasonStats.assists - a.playerSeasonStats.assists})
-      this.playerMoM = this.players.sort((a,b) => {return b.playerSeasonStats.mom - a.playerSeasonStats.mom})
+      this.playerGoals = [...this.players].sort((a,b) => {return b.playerSeasonStats.goals - a.playerSeasonStats.goals})
+      this.playerAssists = [...this.players].sort((a,b) => {return b.playerSeasonStats.assists - a.playerSeasonStats.assists})
+      this.playerMoM = [...this.players].sort((a,b) => {return b.playerSeasonStats.mom - a.playerSeasonStats.mom})
     }, 
     error => {
       console.log(error)
