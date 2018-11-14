5e477f682be8dea61fbfdbb206e45f40b3d65847
Stephen.p.agnew <Stephen.p.agnew@gmail.com>
2018-11-14 15:16:10 +0000

    twitter added and stats sorted

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -10,6 +10,9 @@ import { Player } from './player';
 export class StatsComponent implements OnInit {
 
   players: Player[];
+  playerGoals: Player[];
+  playerAssists: Player[];
+  playerMoM: Player[];
   constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
@@ -20,6 +23,9 @@ export class StatsComponent implements OnInit {
     this.playerService.getPlayers()
     .subscribe(res => {
       this.players = res;
+      this.playerGoals = this.players.sort((a,b) => {return b.playerSeasonStats.goals - a.playerSeasonStats.goals})
+      this.playerAssists = this.players.sort((a,b) => {return b.playerSeasonStats.assists - a.playerSeasonStats.assists})
+      this.playerMoM = this.players.sort((a,b) => {return b.playerSeasonStats.mom - a.playerSeasonStats.mom})
     }, 
     error => {
       console.log(error)
