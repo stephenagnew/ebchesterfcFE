466738f57af1e7015750a51869240b97c9a13e2c
Stephen.p.agnew <Stephen.p.agnew@gmail.com>
2018-11-23 14:42:34 +0000

    auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthService } from './auth.service';
+import { AuthService } from './shared/auth.service';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StatsComponent } from './stats/stats.component';
 import { AngularDraggableModule } from 'angular2-draggable';
@@ -14,6 +14,8 @@ import { MatchesComponent } from './matches/matches.component';
 import { MatchesService } from './matches/matches.service';
 import { PlayerService } from './stats/player.service';
 import { FooterComponent } from './footer/footer.component';
+import { AuthGuardService } from './shared/auth-guard.service';
+import { AuthInterceptor } from './shared/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,14 @@ import { FooterComponent } from './footer/footer.component';
   providers: [
     AuthService,
     MatchesService,
-    PlayerService
+    PlayerService, 
+    AuthGuardService,
+    HttpClientModule,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
   ],
   bootstrap: [AppComponent]
 })
