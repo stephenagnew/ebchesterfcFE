bcc89aceb6ccf0820bcf67b86bbab9e1c89e569e
Stephen.p.agnew <Stephen.p.agnew@gmail.com>
2018-11-30 14:18:42 +0000

    e

--- src/app/shared/endpoints.ts
+++ src/app/shared/endpoints.ts
@@ -1,2 +1,2 @@
-//export const EndpointUrl = "https://ebchesterapi.azurewebsites.net/api";
-export const EndpointUrl = "https://localhost:44352/api";
+export const EndpointUrl = "https://ebchesterapi.azurewebsites.net/api";
+//export const EndpointUrl = "https://localhost:44352/api";
